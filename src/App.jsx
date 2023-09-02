5f380f86e0ecd54b42d9865a2a10a49c1f7d41ba
JuanPabloRP <jpabloroldanp@gmail.com>
2023-09-02 16:40:43 -0500

    fix: import Layout at App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './layouts/layout';
+import Layout from './layouts/Layout';
 import Home from './pages/Home';
 import Pokemon from './pages/Pokemon';
 import NoMatch from './components/NoMatch';
